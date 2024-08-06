709e68db635341f596a985eda4944437342edc7d
helena <helenaff13@gmail.com>
2024-08-06 17:52:15 -0300

    fixed style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import TaskList from "./TaskList";
 import TaskForm from "./TaskForm";
+import NewsFeed from "./NewsFeed";
 import "./App.css";
 
 const App = () => {
@@ -11,9 +12,11 @@ const App = () => {
     title: "",
     completed: false,
   });
+  const [stories, setStories] = useState([]);
 
   useEffect(() => {
     fetchTasks();
+    fetchTopStories();
   }, []);
 
   const fetchTasks = async () => {
@@ -23,6 +26,25 @@ const App = () => {
       setTasks(data);
     } catch (error) {
       console.error("Erro ao buscar tarefas:", error);
+    }
+  };
+
+  const fetchTopStories = async () => {
+    try {
+      const response = await fetch(
+        "https://hacker-news.firebaseio.com/v0/topstories.json"
+      );
+      const storyIds = await response.json();
+      const storiesPromises = storyIds.slice(0, 10).map(async (id) => {
+        const storyResponse = await fetch(
+          `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+        );
+        return await storyResponse.json();
+      });
+      const storiesData = await Promise.all(storiesPromises);
+      setStories(storiesData.slice(0, 6)); // Limit to the first 5 stories
+    } catch (error) {
+      console.error("Erro ao buscar histórias:", error);
     }
   };
 
@@ -78,16 +100,46 @@ const App = () => {
     });
   };
 
+  const toggleCompleted = async (task) => {
+    try {
+      task.completed = !task.completed;
+      const response = await fetch(
+        `http://localhost:8090/api/tasks/${task.id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(task),
+        }
+      );
+      const data = await response.json();
+      setTasks(tasks.map((task) => (task.id === data.id ? data : task)));
+      setIsEditing(false);
+    } catch (error) {
+      console.error("Erro ao atualizar tarefa:", error);
+    }
+  };
+
   return (
-    <div className="app">
-      <h1>Todo List</h1>
-      <TaskForm
-        addTask={addTask}
-        isEditing={isEditing}
-        currentTask={currentTask}
-        updateTask={updateTask}
-      />
-      <TaskList tasks={tasks} deleteTask={deleteTask} editTask={editTask} />
+    <div className="app-container">
+      <div className="app">
+        <h1>Tasks</h1>
+        <TaskForm
+          addTask={addTask}
+          isEditing={isEditing}
+          currentTask={currentTask}
+          updateTask={updateTask}
+        />
+        <TaskList
+          tasks={tasks}
+          deleteTask={deleteTask}
+          editTask={editTask}
+          toggleCompleted={toggleCompleted}
+        />
+      </div>
+      <footer className="app">
+        <h2>Hacker News Stories</h2>
+        <NewsFeed stories={stories} />
+      </footer>
     </div>
   );
 };
